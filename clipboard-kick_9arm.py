00e72da1d4aa4d3e74ae933f7914182059335f8a
Rakprai Kraisungnern <tonkaow@Rakprais-MacBook-Pro.local>
2021-06-17 00:09:22 +0700

    auto add command via clipboard module

---
+++ clipboard-kick_9arm.py
@@ -0,0 +1,11 @@
+import clipboard  as pc
+import time
+
+while (True) :
+    time.sleep(1)
+    a = pc.paste()
+    z = a.split(' ')
+    if z[0] != "!kick" :
+        b = "!kick " + a
+        pc.copy(b)
+        print("Copied %s" % b)
